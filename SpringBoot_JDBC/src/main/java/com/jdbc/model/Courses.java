fb6088413834ff38455c154b08b87a908914c282
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-10-26 12:10:34 -0500

    added Courses Layer

---
+++ SpringBoot_JDBC/src/main/java/com/jdbc/model/Courses.java
@@ -0,0 +1,18 @@
+package com.jdbc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Courses {
+
+	private int course_id;
+	
+	private String course_name;
+	
+}
