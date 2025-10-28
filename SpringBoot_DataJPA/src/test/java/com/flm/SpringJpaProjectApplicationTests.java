e9819e5e36281de063d4881ceed98582d5bb4fbe
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-10-27 20:15:59 -0500

    created SpringBoot DataJPA project

---
+++ SpringBoot_DataJPA/src/test/java/com/flm/SpringJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
