dce8065c15eed9ab3c9a120ff8d4b1f1676cd332
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-02 22:10:10 -0500

    Initialize Spring Boot project with security and JPA

---
+++ SpringSecurityInDB/src/main/java/com/sec/SpringSecurityInDbApplication.java
@@ -0,0 +1,13 @@
+package com.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInDbApplication.class, args);
+	}
+
+}
