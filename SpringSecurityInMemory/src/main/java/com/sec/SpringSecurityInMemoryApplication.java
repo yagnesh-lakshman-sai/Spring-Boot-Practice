ee854eeec807663bfef5069ac255fb790101df75
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-03 23:25:16 -0500

    Initialize SpringSecurityInMemory project
    .

---
+++ SpringSecurityInMemory/src/main/java/com/sec/SpringSecurityInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInMemoryApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityInMemory/src/test/java/com/sec/SpringSecurityInMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
