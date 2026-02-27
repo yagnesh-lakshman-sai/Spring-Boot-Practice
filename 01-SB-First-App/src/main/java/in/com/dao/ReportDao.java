9bd28ff573c11c291c0ba2d036b48f0b1b441155
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-02-27 23:54:16 +0530

    Add ReportDao repository class

---
+++ 01-SB-First-App/src/main/java/in/com/dao/ReportDao.java
@@ -0,0 +1,11 @@
+package in.com.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReportDao {
+	
+	public ReportDao() {
+		System.out.println("ReportDao::Constructor");
+	}
+}
