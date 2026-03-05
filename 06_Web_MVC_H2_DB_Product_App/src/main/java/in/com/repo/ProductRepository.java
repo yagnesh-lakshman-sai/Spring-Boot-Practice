851f2579b5b0ac3bbb31c306ea47b16b2c15181b
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-03-05 18:54:11 +0530

    Add ProductRepository JPA interface

---
+++ 06_Web_MVC_H2_DB_Product_App/src/main/java/in/com/repo/ProductRepository.java
@@ -0,0 +1,9 @@
+package in.com.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.com.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+}
