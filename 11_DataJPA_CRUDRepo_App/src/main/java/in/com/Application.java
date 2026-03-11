6991df1b302b990b96366804a08e5c5280b55e3c
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-03-11 23:22:10 +0530

    add CRUDRepo

---
+++ 11_DataJPA_CRUDRepo_App/src/main/java/in/com/Application.java
@@ -0,0 +1,34 @@
+package in.com;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import in.com.entity.Book;
+import in.com.repo.BookRepository;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctxt = SpringApplication.run(Application.class, args);
+
+		BookRepository repo = ctxt.getBean(BookRepository.class);
+
+		List<Book> books = repo.getAllBooks();
+
+		for (Book b : books) {
+			System.out.println(b);
+		}
+		System.out.println("====================");
+		
+		repo.getBooks();
+	}
+}
+
+
+
+
+
