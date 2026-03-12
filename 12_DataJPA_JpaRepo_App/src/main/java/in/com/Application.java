f891ff56146c85b89821d41f8648fda01069c09f
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-03-12 13:47:32 +0530

    add Application Class

---
+++ 12_DataJPA_JpaRepo_App/src/main/java/in/com/Application.java
@@ -0,0 +1,39 @@
+package in.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import in.com.entity.Employee;
+import in.com.repo.EmployeeRepository;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+		EmployeeRepository repository = context.getBean(EmployeeRepository.class);
+
+		Employee emp = new Employee();
+		
+		emp.setEmpName("Mohan");
+		emp.setDept("Sales");
+		emp.setEmpGender("Male");
+		
+		emp = repository.save(emp);
+		
+		System.out.println("record inserted...");
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
