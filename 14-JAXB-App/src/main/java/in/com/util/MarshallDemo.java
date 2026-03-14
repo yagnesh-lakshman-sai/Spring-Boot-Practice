c34eab3a709e3d047959d11f932738ebfc8c1589
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-03-14 20:35:23 +0530

    add Marshall Demo

---
+++ 14-JAXB-App/src/main/java/in/com/util/MarshallDemo.java
@@ -0,0 +1,35 @@
+package in.com.util;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+import in.com.binding.Address;
+import in.com.binding.Person;
+
+public class MarshallDemo {
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		Address addr = new Address();
+		addr.setCity("Hyd");
+		addr.setState("TG");
+		addr.setCountry("India");
+
+		Person p = new Person();
+		p.setId(101);
+		p.setName("Ashok");
+		p.setGender("Male");
+		p.setEmail("ashokitschool@gmail.com");
+		p.setAddr(addr);
+
+		JAXBContext context = JAXBContext.newInstance(Person.class);
+		Marshaller marshaller = context.createMarshaller();
+		marshaller.marshal(p, new File("person.xml"));
+		
+		System.out.println("done.....");
+		
+	}
+}
