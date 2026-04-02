7945bb63baf5b2627c6dcd7f70ba0b010b3f0f93
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-02 17:26:31 +0530

    add Ticket Class

---
+++ 18_SB_REST_MakeMyTrip_App/src/main/java/com/sbms/response/Ticket.java
@@ -0,0 +1,14 @@
+package com.sbms.response;
+
+import lombok.Data;
+
+@Data
+public class Ticket {
+
+	private Integer ticketId;
+	private String from;
+	private String to;
+	private String ticketStatus;
+	private String tktCost;
+	private String trainNum;
+}
