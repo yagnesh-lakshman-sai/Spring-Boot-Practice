eb6b8b266f6777fe696d2c46a51b6b3468981b24
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-03 22:36:02 +0530

    add Passenger Class

---
+++ 18_SB_REST_MakeMyTrip_App/src/main/java/com/sbms/request/Passenger.java
@@ -0,0 +1,15 @@
+package com.sbms.request;
+
+import lombok.Data;
+
+@Data
+public class Passenger {
+
+	private String fname;
+	private String lname;
+	private String doj;
+	private String from;
+	private String to;
+	private String trainNum;
+
+}
