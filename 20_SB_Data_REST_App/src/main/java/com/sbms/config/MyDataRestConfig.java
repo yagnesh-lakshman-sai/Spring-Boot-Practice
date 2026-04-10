172e1503928cc888855ea90ccb13ea3e37911629
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-10 19:17:46 +0530

    add MyDataRestConfig Class

---
+++ 20_SB_Data_REST_App/src/main/java/com/sbms/config/MyDataRestConfig.java
@@ -0,0 +1,26 @@
+package com.sbms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.sbms.entity.Book;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+	
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+		HttpMethod[] unsupportedMethods = { HttpMethod.PUT, HttpMethod.DELETE };
+		
+		config.getExposureConfiguration()
+			  .forDomainType(Book.class)
+			  .withItemExposure((metadata, http) -> http.disable(unsupportedMethods))
+			  .withCollectionExposure((metadata,http) -> http.disable(unsupportedMethods));
+		
+	}
+}
