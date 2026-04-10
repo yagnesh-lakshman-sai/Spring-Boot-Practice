5e1cc6b6f4dcd1423937f578b07872ee4b82abc2
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-10 19:18:22 +0530

    add BookRepository Interface

---
+++ 20_SB_Data_REST_App/src/main/java/com/sbms/repo/BookRepository.java
@@ -0,0 +1,14 @@
+package com.sbms.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.sbms.entity.Book;
+
+@RepositoryRestResource(path = "books")
+public interface BookRepository extends JpaRepository<Book, Integer> {
+	public List<Book> findByNameContaining(@Param("name") String name);
+}
