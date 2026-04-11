5a38eebaa670bcbca1fcb5ffdca5b899429e1c5a
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-11 19:51:20 +0530

    add CustomerNotFoundException  Class

---
+++ 21_REST_API_Exception_Handling/src/main/java/com/sbms/exception/CustomerNotFoundException.java
@@ -0,0 +1,13 @@
+package com.sbms.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+	public CustomerNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
