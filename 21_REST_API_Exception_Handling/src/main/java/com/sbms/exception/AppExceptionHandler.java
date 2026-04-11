05b7192d66b2e060f3f26aaa3ad44b4fdf5e93fc
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-11 19:51:07 +0530

    add AppExceptionHandler Class

---
+++ 21_REST_API_Exception_Handling/src/main/java/com/sbms/exception/AppExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.sbms.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AppExceptionHandler {
+
+	@ExceptionHandler(value = CustomerNotFoundException.class)
+	public ResponseEntity<ExceptionInfo> handleCnfe(CustomerNotFoundException cnfe) {
+
+		ExceptionInfo info = new ExceptionInfo();
+		info.setCode("EX0011");
+		info.setMsg(cnfe.getMessage());
+		info.setDate(LocalDateTime.now());
+		
+		return new ResponseEntity<>(info, HttpStatus.BAD_REQUEST);
+
+	}
+}
