b1b54bb140c68844c39dd64d03b189fe1f2bb477
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-12 21:16:03 +0530

    add CustomerRestController  Class

---
+++ 21_REST_API_Exception_Handling/src/main/java/com/sbms/rest/CustomerRestController.java
@@ -0,0 +1,21 @@
+package com.sbms.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sbms.service.CustomerService;
+
+@RestController
+public class CustomerRestController {
+
+	@Autowired
+	private CustomerService service;
+
+	@GetMapping("/customer/{customerId}")
+	public String getCustomerName(@PathVariable Integer customerId) throws Exception {
+		return service.getCustomerNameById(customerId);
+	}
+	
+}
