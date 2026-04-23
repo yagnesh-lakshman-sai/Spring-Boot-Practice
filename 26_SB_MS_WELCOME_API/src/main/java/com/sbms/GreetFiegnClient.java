d8a02f097fd96ec8cd4e284c7cfea130f26f2235
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-23 21:19:48 +0530

    add greetFieegnClient

---
+++ 26_SB_MS_WELCOME_API/src/main/java/com/sbms/GreetFiegnClient.java
@@ -0,0 +1,12 @@
+package com.sbms;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "GREET-API")
+public interface GreetFiegnClient {
+
+	@GetMapping("/greet")
+	public String invokGreetApi();
+
+}
