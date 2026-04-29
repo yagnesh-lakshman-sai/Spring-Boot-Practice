5cfa0d4f50c8bb01d2bf718ed1c72defc37cd43e
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-29 19:00:32 +0530

    initialize SB_MS_Config_server_Client_App

---
+++ 31_SB_MS_Config_Server_Client_App/src/main/java/com/sbms/Application.java
@@ -0,0 +1,14 @@
+package com.sbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
