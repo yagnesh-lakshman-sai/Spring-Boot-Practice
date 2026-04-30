4068f62ee23e26a051a7eca1a5142df598d8ed06
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-04-30 22:24:29 +0530

    add MsgController

---
+++ 32_Spring_Security_Basic_Auth/src/main/java/com/sbms/rest/MsgController.java
@@ -0,0 +1,14 @@
+package com.sbms.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MsgController {
+	
+	@GetMapping("/")
+	public String getMsg() {
+		return "Hello World";
+	}
+
+}
